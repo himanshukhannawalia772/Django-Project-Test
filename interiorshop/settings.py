0b74a531c6c4f7fe7888cff03c27e8e4ec4fde73
himanshukhannawalia772 <85253153+himanshukhannawalia772@users.noreply.github.com>
2022-01-31 16:47:54 +0530

    Fourth Changes

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m&zp2zb1&3v^19&lnebn07_@cy!hf&roasy^(o%*!)78&9^16i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','django-multi-vendor.herokuapp.com']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
